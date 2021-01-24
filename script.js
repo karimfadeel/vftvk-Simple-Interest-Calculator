c26ca5088ab5e8d4e6fde6880b81b37bb1e4b799
Karim Fadeel Hassanin <38159609+karimfadeel@users.noreply.github.com>
2021-01-24 15:50:35 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+            function compute()
+            {
+  
+  var d = new Date();
+  var y = d.getFullYear();
+  var s = 0
+
+               	p = document.getElementById("principal").value;
+                n = document.getElementById("years").value;
+                r = document.getElementById("rate").value;
+                result = document.getElementById("result");
+                resultn = document.getElementById("resultn");
+                resultp = document.getElementById("resultp");
+                resultr = document.getElementById("resultr");
+				s = Number(n) + Number(y);
+           		resultp.innerHTML = "If you deposit " + (p); 
+                resultr.innerHTML = "at an interest rate of " + (r); 
+                result.innerHTML = " You will receive an amount of " + (p*n*r/100); 
+                resultn.innerHTML = "in the year " + (s);
+            }
+
+
+var svalue = document.getElementById("rate");
+var output = document.getElementById("rvalue");
+output.innerHTML = svalue.value;
+
+svalue.oninput = function() {
+output.innerHTML = this.value;
 }
-        
